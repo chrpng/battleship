624a9d2d615ccef1f89a13d0ffd270e13a7a9a9e
chrpng <chrpng.brd@gmail.com>
2020-04-02 19:03:30 -0400

    Updates

---
+++ precache-manifest.634e2ff0e25608bd094b32dece67b9e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60d9876c375973fc2e3e18f4c04b4f2",
+    "url": "/odin-battleship/index.html"
+  },
+  {
+    "revision": "908f2ebf5db22b8771e9",
+    "url": "/odin-battleship/static/css/main.e7b5ded8.chunk.css"
+  },
+  {
+    "revision": "318e027604a99ec78c29",
+    "url": "/odin-battleship/static/js/2.e278d6b4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/odin-battleship/static/js/2.e278d6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908f2ebf5db22b8771e9",
+    "url": "/odin-battleship/static/js/main.cb19f6c3.chunk.js"
+  },
+  {
+    "revision": "e19e22523a67d6d9b62d",
+    "url": "/odin-battleship/static/js/runtime-main.5d0309a3.js"
+  }
+]);
